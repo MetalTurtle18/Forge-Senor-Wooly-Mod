7a1a5400f4833e64f125449463055c36ada96186
dorian <dekolis1218@gmail.com>
2020-07-25 15:46:10 -0700

    Create loot table for Magic Wool so it now drops itself

--- src/main/java/com/metalturtle/srwooly/blocks/MagicWool.java
+++ src/main/java/com/metalturtle/srwooly/blocks/MagicWool.java
@@ -1,15 +1,16 @@
 package com.metalturtle.srwooly.blocks;
 
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
-import net.minecraftforge.common.ToolType;
+import net.minecraftforge.common.IForgeShearable;
 
-public class MagicWool extends Block {
+public class MagicWool extends Block implements IForgeShearable {
 
     public MagicWool() {
-        super(Block.Properties.create(Material.WOOL)
-                .hardnessAndResistance(0.8f, 0.8f)
+        super(AbstractBlock.Properties.create(Material.WOOL)
+                .hardnessAndResistance(0.8f)
                 .sound(SoundType.CLOTH)
                 .harvestLevel(0)
                 .setLightLevel(value -> 15));
