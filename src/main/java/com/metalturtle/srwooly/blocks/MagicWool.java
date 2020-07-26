37c69d0f3873586d1e7354991b5dff51f6578631
dorian <dekolis1218@gmail.com>
2020-07-26 09:32:32 -0700

    Make magic_wool break faster when broken by shears

--- src/main/java/com/metalturtle/srwooly/blocks/MagicWool.java
+++ src/main/java/com/metalturtle/srwooly/blocks/MagicWool.java
@@ -1,15 +1,13 @@
 package com.metalturtle.srwooly.blocks;
 
-import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
-import net.minecraftforge.common.IForgeShearable;
 
-public class MagicWool extends Block implements IForgeShearable {
+public class MagicWool extends Block {
 
     public MagicWool() {
-        super(AbstractBlock.Properties.create(Material.WOOL)
+        super(Block.Properties.create(Material.WOOL)
                 .hardnessAndResistance(0.8f)
                 .sound(SoundType.CLOTH)
                 .harvestLevel(0)
