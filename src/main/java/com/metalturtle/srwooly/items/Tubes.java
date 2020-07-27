eb6cbd8ce8402c78f74ec2e0bd1a8702763e2e37
dorian <dekolis1218@gmail.com>
2020-07-27 12:21:27 -0700

    Use supplier for .effect to make it happy

--- src/main/java/com/metalturtle/srwooly/items/Tubes.java
+++ src/main/java/com/metalturtle/srwooly/items/Tubes.java
@@ -7,14 +7,14 @@ import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 
 public class Tubes extends Item {
-
+    
     public Tubes() {
         super(new Item.Properties()
                 .group(SenorWooly.TAB)
                 .food(new Food.Builder()
                         .hunger(4)
                         .saturation(1.2f)
-                        .effect(new EffectInstance(Effects.ABSORPTION, 80, 34), 1)
+                        .effect(() -> new EffectInstance(Effects.ABSORPTION, 80, 34), 1)
                         .setAlwaysEdible()
                         .build())
         );
